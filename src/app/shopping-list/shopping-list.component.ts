cd3359a00286aeb4f8604c73283e0496568f4d3b
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2021-12-03 19:23:20 +0530

    added the ingridient list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/model/ingridient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingridients: Ingridient[] = [
+    new Ingridient('Apples', 5),
+    new Ingridient('Tometoes', 10),
+  ]; //
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
