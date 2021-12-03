8aead0af45be9d5e719af8add2b76a34550700e8
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2021-12-03 18:46:20 +0530

    added the recipe list

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Paneer Tikka Masala',
+      'Recipe to prepare Panner Tikka Masala',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2014/11/paneer-tikka-masala-recipe-1-500x500.jpg'
+    ),
+    new Recipe(
+      'Chiken Tikka',
+      'Recipe to prepare Chiken Tikka',
+      'https://www.awesomecuisine.com/wp-content/uploads/2014/03/chicken-tikka.jpg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
