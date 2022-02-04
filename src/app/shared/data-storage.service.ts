e16f7dfee68f2e37bff6cf2739b3bbc861f3dabe
Bhushan Abhisheki <bhushanabhisheki@gmail.com>
2022-02-04 15:27:33 +0530

    Storing Recipes on firebase

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipe/recipe.model';
+import { RecipeService } from '../recipe/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes: Recipe[] = this.recipeService.getRecipes();
+
+    return this.http
+      .put(
+        'https://recipebook-f6251-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log('Response: ' + response);
+      });
+  }
+}
